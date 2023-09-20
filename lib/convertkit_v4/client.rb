4249e1ed49a2b5e7428e90303464093877b8383c
Petar <risteskipetar3@gmail.com>
2023-09-20 10:35:52 -0400

    Access token and refresh token

--- lib/convertkit_v4/client.rb
+++ lib/convertkit_v4/client.rb
@@ -20,6 +20,7 @@ module ConvertkitV4
     attr_accessor :access_token, :refresh_token, :client_id, :client_secret, :redirect_uri
 
     AUTH_URL = "https://app.convertkit.com/oauth/authorize"
+    ACCESS_TOKEN_URL = "https://app.convertkit.com/oauth/token"
 
     def initialize(access_token: nil, refresh_token: nil, client_id: nil, client_secret: nil, redirect_uri: nil)
       @access_token = access_token
@@ -38,6 +39,31 @@ module ConvertkitV4
       Faraday.new(url: AUTH_URL).get do |req|
         req.headers["Content-Type"] = "application/json"
         req.params = { client_id: @client_id, redirect_uri: @redirect_uri }
+      end
+    end
+
+    def get_access_token(code)
+      Faraday.new(url: ACCESS_TOKEN_URL).post do |req|
+        req.headers["Content-Type"] = "application/json"
+        req.params = {
+          client_id: @client_id,
+          client_secret: @client_secret,
+          code: code,
+          grant_type: "authorization_code",
+          redirect_uri: @redirect_uri
+        }
+      end
+    end
+
+    def refresh_token
+      Faraday.new(url: ACCESS_TOKEN_URL).post do |req|
+        req.headers["Content-Type"] = "application/json"
+        req.params = {
+          client_id: @client_id,
+          client_secret: @client_secret,
+          refresh_token: @refresh_token,
+          grant_type: "refresh_token"
+        }
       end
     end
   end
