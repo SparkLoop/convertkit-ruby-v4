93429b4b2698ecc2ae0796be670c9b2e27dc90be
Manuel Frigerio <manuel@friger.io>
2023-09-25 21:58:52 +0100

    Update subscribers.rb

--- lib/convertkit_v4/client/subscribers.rb
+++ lib/convertkit_v4/client/subscribers.rb
@@ -9,15 +9,28 @@ module ConvertkitV4
         connection.get("subscribers/#{subscriber_id}")
       end
 
+      def add_subscribers(options = {})
+        response = connection.post("subscribers") do |f|
+          f.body = JSON.generate({
+            first_name: options[:first_name],
+            email_address: options[:email_address]
+          })
+        end
+        response.body
+      end
+
       def subscriber_tags(subscriber_id)
         connection.get("subscribers/#{subscriber_id}/tags")
       end
 
       def update_subscriber(subscriber_id, options = {})
         response = connection.put("subscribers/#{subscriber_id}") do |f|
-          f.params["email_address"] = options[:email_address] if options[:email_address]
-          f.params["fields"] = options[:fields] if options[:fields]
-          f.params["first_name"] = options[:first_name] if options[:first_name]
+          body = {}
+          body[:email_address] = options[:email_address] if options[:email_address]
+          body[:fields] = options[:fields] if options[:fields]
+          body[:first_name] = options[:first_name] if options[:first_name]
+
+          f.body = JSON.generate(body)
         end
         response.body
       end
