1cdbffe49a204b1e9c7bb4dbadc4e4a3df28c143
Manuel Frigerio <manuel@friger.io>
2023-09-25 22:02:15 +0100

    Update custom_fields.rb

--- lib/convertkit_v4/client/custom_fields.rb
+++ lib/convertkit_v4/client/custom_fields.rb
@@ -8,7 +8,9 @@ module ConvertkitV4
 
       def add_custom_field(options = {})
         connection.post("custom_fields") do |f|
-          f.params['label'] = options[:label]
+          f.body = JSON.generate({
+            label: options[:label]
+          })
         end
       end
 
@@ -18,7 +20,9 @@ module ConvertkitV4
 
       def update_custom_field(custom_field_id, options = {})
         connection.put("custom_fields/#{custom_field_id}") do |f|
-          f.params['label'] = options[:label]
+          f.body = JSON.generate({
+            label: options[:label]
+          })
         end
       end
 
