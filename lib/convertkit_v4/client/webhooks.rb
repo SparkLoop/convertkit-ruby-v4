c57d86b831b53a963c869f947ee0286b2103372f
Manuel Frigerio <manuel@friger.io>
2023-09-25 21:26:34 +0100

    Update webhooks.rb

--- lib/convertkit_v4/client/webhooks.rb
+++ lib/convertkit_v4/client/webhooks.rb
@@ -2,19 +2,15 @@ module ConvertkitV4
   class Client
     module Webhooks
       def webhooks
-        connection.get("automations/hooks/").body
+        connection.get("webhooks").body
       end
 
       def create_webhook(url, events)
-        response = connection.post("automations/hooks") do |f|
+        response = connection.post("webhooks") do |f|
           f.params['target_url'] = url
           f.params['event'] = events
         end
         response.body
-      end
-
-      def remove_webhook(rule_id)
-        connection.delete("automations/hooks/#{rule_id}").body
       end
     end
   end
