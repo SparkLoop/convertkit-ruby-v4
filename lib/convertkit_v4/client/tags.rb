f71338237a6c3b558a49d1aa73dd89fef98c7bba
Manuel Frigerio <manuel@friger.io>
2023-09-25 22:01:14 +0100

    Update tags.rb

--- lib/convertkit_v4/client/tags.rb
+++ lib/convertkit_v4/client/tags.rb
@@ -6,7 +6,7 @@ module ConvertkitV4
       end
 
       def add_subscriber_to_tag(tag_id, email, options = {})
-        connection.post("tags/#{tag_id}/subscribe") do |f|
+        connection.post("tags/#{tag_id}/subscribers") do |f|
           f.params['email'] = email
           f.params['first_name'] = options[:first_name]
           f.params['fields'] = options[:fields]
@@ -20,30 +20,21 @@ module ConvertkitV4
 
       def remove_tag_from_subscriber_by_email(tag_id, email)
         connection.post("tags/#{tag_id}/unsubscribe") do |f|
-          f.params['email'] = email
+          f.body = JSON.generate({
+            email: email
+          })
         end
       end
 
       def create_tag(tag_name)
         response = connection.post("tags") do |request|
-          request.params["tag"] = { name: tag_name }
+          f.body = JSON.generate({
+            tag: {
+              name: tag_name
+            }
+          })
         end
         response.body
-      end
-
-      def create_tags(tag_names)
-        response = connection.post("tags") do |request|
-          request.params["tag"] = tag_names.map { |tag_name| { name: tag_name } }
-        end
-        response.body
-      end
-
-      def subscriptions_to_tag(tag_id, options = {})
-        connection.get("tags/#{tag_id}/subscriptions", options) do |f|
-          f.params["page"] = options[:page] if options[:page]
-          f.params["sort_order"] = options[:sort_order] if options[:sort_order]
-          f.params["subscriber_state"] = options[:subscriber_state] if options[:subscriber_state]
-        end
       end
     end
   end
