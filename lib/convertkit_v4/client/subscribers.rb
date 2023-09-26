8d31572b60355f9d5ab246f0013c0377717ec7cd
Manuel Frigerio <manuel.frigerio@gmail.com>
2023-09-26 12:33:51 +0100

    subscribers

--- lib/convertkit_v4/client/subscribers.rb
+++ lib/convertkit_v4/client/subscribers.rb
@@ -1,6 +1,7 @@
 module ConvertkitV4
   class Client
     module Subscribers
+
       def subscribers(options = {})
         connection.get("subscribers", options)
       end
@@ -9,27 +10,42 @@ module ConvertkitV4
         connection.get("subscribers/#{subscriber_id}")
       end
 
-      def subscriber_tags(subscriber_id)
-        connection.get("subscribers/#{subscriber_id}/tags")
-      end
+      def add_subscriber(email_address, options = {})
+        response = connection.post("subscribers") do |f|
+          body = {
+            email_address: email_address
+          }
 
-      def update_subscriber(subscriber_id, options = {})
-        response = connection.put("subscribers/#{subscriber_id}") do |f|
-          f.params["email_address"] = options[:email_address] if options[:email_address]
-          f.params["fields"] = options[:fields] if options[:fields]
-          f.params["first_name"] = options[:first_name] if options[:first_name]
+          body[:fields] = options[:fields] if options[:fields]
+          body[:first_name] = options[:first_name] if options[:first_name]
+
+          f.body = JSON.generate(body)
         end
         response.body
       end
 
-      def unsubscribe(email)
-        connection.put("unsubscribe") do |f|
-          f.params['email'] = email
+      def update_subscriber(subscriber_id, options = {})
+        response = connection.put("subscribers/#{subscriber_id}") do |f|
+          body = {}
+          body[:email_address] = options[:email_address] if options[:email_address]
+          body[:fields] = options[:fields] if options[:fields]
+          body[:first_name] = options[:first_name] if options[:first_name]
+
+          f.body = JSON.generate(body)
+        end
+        response.body
+      end
+
+      def unsubscribe(subscriber_id)
+        connection.post("unsubscribe") do |f|
+          f.body = JSON.generate({
+            id: subscriber_id
+          })
         end
       end
 
-      def remove_tag_from_subscriber(subscriber_id, tag_id)
-        connection.delete("subscribers/#{subscriber_id}/tags/#{tag_id}")
+      def subscriber_tags(subscriber_id)
+        connection.get("subscribers/#{subscriber_id}/tags")
       end
     end
   end
