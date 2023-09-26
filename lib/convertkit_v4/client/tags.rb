6c012b69aa488fa20dec3abf336014b6ba7f0f4f
Manuel Frigerio <manuel.frigerio@gmail.com>
2023-09-26 12:36:09 +0100

    tags

--- lib/convertkit_v4/client/tags.rb
+++ lib/convertkit_v4/client/tags.rb
@@ -1,50 +1,29 @@
 module ConvertkitV4
   class Client
     module Tags
+
       def tags
         connection.get("tags")
       end
 
-      def add_subscriber_to_tag(tag_id, email, options = {})
-        connection.post("tags/#{tag_id}/subscribe") do |f|
-          f.params['email'] = email
-          f.params['first_name'] = options[:first_name]
-          f.params['fields'] = options[:fields]
-          f.params['tags'] = options[:tags]
-        end
-      end
-
-      def remove_tag_from_subscriber(tag_id, subscriber_id)
-        connection.delete("subscribers/#{subscriber_id}/tags/#{tag_id}")
-      end
-
-      def remove_tag_from_subscriber_by_email(tag_id, email)
-        connection.post("tags/#{tag_id}/unsubscribe") do |f|
-          f.params['email'] = email
-        end
-      end
-
       def create_tag(tag_name)
-        response = connection.post("tags") do |request|
-          request.params["tag"] = { name: tag_name }
+        response = connection.post("tags") do |f|
+          f.body = JSON.generate({
+            name: tag_name
+          })
         end
         response.body
       end
 
-      def create_tags(tag_names)
-        response = connection.post("tags") do |request|
-          request.params["tag"] = tag_names.map { |tag_name| { name: tag_name } }
+      def add_subscriber_to_tag(tag_id, options = {})
+        connection.post("tags/#{tag_id}/subscribers") do |f|
+          f.body = JSON.generate({
+            email: options[:email],
+            id: options[:id]
+          })
         end
-        response.body
       end
 
-      def subscriptions_to_tag(tag_id, options = {})
-        connection.get("tags/#{tag_id}/subscriptions", options) do |f|
-          f.params["page"] = options[:page] if options[:page]
-          f.params["sort_order"] = options[:sort_order] if options[:sort_order]
-          f.params["subscriber_state"] = options[:subscriber_state] if options[:subscriber_state]
-        end
-      end
     end
   end
 end
